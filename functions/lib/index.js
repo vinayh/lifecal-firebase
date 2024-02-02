fb7b4c2d4d7c7078fdce84676169d04f996d2f76
Vinay Hiremath <github@vinayh.com>
2024-02-02 20:30:33 +0000

    Added routes to add/delete user doc on Firebase auth trigger

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,25 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUser = exports.deleteUser = exports.addUser = void 0;
+exports.getUser = exports.editUserProfile = exports.deleteUser = exports.addUser = void 0;
 const zod_1 = require("zod");
 // import { readFileSync } from 'fs';
 // import * as logger from "firebase-functions/logger";
 const https_1 = require("firebase-functions/v2/https");
+const functions = require("firebase-functions/v1");
 const firestore_1 = require("firebase-admin/firestore");
 const app_1 = require("firebase-admin/app");
 const auth_1 = require("firebase-admin/auth");
 (0, app_1.initializeApp)();
 const db = {
-    users: (0, firestore_1.getFirestore)().collection('users')
+    users: (0, firestore_1.getFirestore)().collection("users"),
 };
 const TagZ = zod_1.z.object({
-    id: zod_1.z.number(), created: zod_1.z.date(), name: zod_1.z.string(), color: zod_1.z.string()
+    id: zod_1.z.number(), created: zod_1.z.date(), name: zod_1.z.string(), color: zod_1.z.string(),
 });
 const EntryZ = zod_1.z.object({
-    id: zod_1.z.number(), created: zod_1.z.date(), start: zod_1.z.date(), note: zod_1.z.string(), tags: zod_1.z.array(TagZ)
+    id: zod_1.z.number(), created: zod_1.z.date(), start: zod_1.z.date(), note: zod_1.z.string(), tags: zod_1.z.array(TagZ),
 });
 const UserZ = zod_1.z.object({
-    uid: zod_1.z.string(), created: zod_1.z.date(), name: zod_1.z.string(), birth: zod_1.z.date(), expYears: zod_1.z.number(), email: zod_1.z.string().email().optional(), entries: zod_1.z.array(EntryZ), tags: zod_1.z.array(TagZ)
+    uid: zod_1.z.string(), created: zod_1.z.date(), name: zod_1.z.string(), birth: zod_1.z.date(), expYears: zod_1.z.number(), email: zod_1.z.string().email().optional(), entries: zod_1.z.array(EntryZ), tags: zod_1.z.array(TagZ),
 });
 // const secretNames: [string] = ["GITHUB_CLIENT_ID"]
 // const secrets = Object.fromEntries(secretNames.map(x => [x, readFileSync(`../.secrets/${x.toLowerCase()}`, 'utf-8')]))
@@ -30,33 +31,47 @@ async function validateUid(request) {
 }
 async function userFromRequest(request) {
     return validateUid(request)
-        .then(uid => db.users.where("uid", "==", uid).limit(1))
-        .then(query => query.get())
-        .then(querySnapshot => querySnapshot.docs[0]);
+        .then((uid) => db.users.where("uid", "==", uid).limit(1))
+        .then((query) => query.get())
+        .then((querySnapshot) => querySnapshot.docs[0]);
 }
 // export const helloWorld = onRequest((request, response) => {
 //     logger.info("Hello logs!", { structuredData: true });
 //     response.send("Hello again !");
 // });
-exports.addUser = (0, https_1.onRequest)(async (request, response) => {
-    const { uid, name, birth, expYears, email } = request.query;
-    var user = { uid: uid, created: new Date(), name: name, birth: new Date(birth), expYears: parseInt(expYears), entries: [], tags: [] };
-    user = UserZ.parse(Object.assign(Object.assign({}, user), (email && { email })));
-    db.users.add(user)
+exports.addUser = functions.auth.user().onCreate(async (user) => {
+    console.log("Add user triggered", user);
+    return await db.users.doc(user.uid).set({ created: new Date() });
+});
+exports.deleteUser = functions.auth.user().onDelete(async (user) => {
+    console.log("Delete user triggered", user);
+    return await db.users.doc(user.uid).delete();
+});
+exports.editUserProfile = (0, https_1.onRequest)(async (request, response) => {
+    const { name, birth, expYears, email } = request.query;
+    validateUid(request)
+        .then((uid) => {
+        return { uid: uid, created: new Date(), name: name, birth: new Date(birth), expYears: parseInt(expYears), entries: [], tags: [] };
+    })
+        .then((newUser) => UserZ.parse(Object.assign(Object.assign({}, newUser), (email && { email }))))
+        .then((newUser) => db.users.add(newUser))
         .then((res) => res.get())
-        .then(user => response.send(user))
-        .catch((error) => console.error("Caught error!", error));
+        .then((user) => response.send(user))
+        .catch((error) => console.error("Error adding user!", error));
 });
-exports.deleteUser = (0, https_1.onRequest)(async (request, response) => {
-    userFromRequest(request)
-        .then(user => user.ref.delete())
-        .then(_ => response.status(200).send("Deleted"))
-        .catch(error => response.status(500).send(`Error ${error}`));
-});
+// export const deleteUser = onRequest(async (request, response) => {
+//     userFromRequest(request)
+//         .then(user => user.ref.delete())
+//         .then(_ => response.status(200).send("Deleted"))
+//         .catch(error => response.status(500).send(`Error ${error}`))
+// });
 exports.getUser = (0, https_1.onRequest)(async (request, response) => {
     userFromRequest(request)
-        .then(user => UserZ.parse(user))
+        .then((user) => {
+        console.log(user);
+        return UserZ.parse(user);
+    })
         .then((user) => response.status(200).send(user))
-        .catch(error => response.status(500).send(`Error ${error}`));
+        .catch((error) => response.status(500).send(`Error ${error}`));
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,32 +1,32 @@
-import { z } from "zod";
+import {z} from "zod";
 // import { readFileSync } from 'fs';
 // import * as logger from "firebase-functions/logger";
-import { Request, onRequest } from "firebase-functions/v2/https";
-import { QueryDocumentSnapshot, DocumentReference } from "firebase-admin/firestore";
-import { getFirestore } from "firebase-admin/firestore";
-import { initializeApp } from "firebase-admin/app";
-import { DecodedIdToken, getAuth } from "firebase-admin/auth"
+import {Request, onRequest} from "firebase-functions/v2/https";
+import * as functions from "firebase-functions/v1";
+import {QueryDocumentSnapshot, DocumentReference, getFirestore} from "firebase-admin/firestore";
+import {initializeApp} from "firebase-admin/app";
+import {DecodedIdToken, getAuth} from "firebase-admin/auth";
 
 initializeApp();
 const db = {
-    users: getFirestore().collection('users')
-}
+  users: getFirestore().collection("users"),
+};
 
 const TagZ = z.object({
-    id: z.number(), created: z.date(), name: z.string(), color: z.string()
-})
+  id: z.number(), created: z.date(), name: z.string(), color: z.string(),
+});
 type Tag = { id: number, created: Date, name: string, color: string }
 
 
 const EntryZ = z.object({
-    id: z.number(), created: z.date(), start: z.date(), note: z.string(), tags: z.array(TagZ)
-})
+  id: z.number(), created: z.date(), start: z.date(), note: z.string(), tags: z.array(TagZ),
+});
 type Entry = { id: number, created: Date, start: Date, note: string, tags: Tag[] }
 
 
 const UserZ = z.object({
-    uid: z.string(), created: z.date(), name: z.string(), birth: z.date(), expYears: z.number(), email: z.string().email().optional(), entries: z.array(EntryZ), tags: z.array(TagZ)
-})
+  uid: z.string(), created: z.date(), name: z.string(), birth: z.date(), expYears: z.number(), email: z.string().email().optional(), entries: z.array(EntryZ), tags: z.array(TagZ),
+});
 type User = { uid: string, created: Date, name: string, birth: Date, expYears: number, email?: string, entries: Entry[], tags: Tag[] }
 
 
@@ -34,16 +34,16 @@ type User = { uid: string, created: Date, name: string, birth: Date, expYears: n
 // const secrets = Object.fromEntries(secretNames.map(x => [x, readFileSync(`../.secrets/${x.toLowerCase()}`, 'utf-8')]))
 
 async function validateUid(request: Request): Promise<string> {
-    const { idToken } = request.query as { idToken: string }
-    return getAuth().verifyIdToken(idToken)
-        .then((decodedToken: DecodedIdToken) => decodedToken.uid)
+  const {idToken} = request.query as { idToken: string };
+  return getAuth().verifyIdToken(idToken)
+    .then((decodedToken: DecodedIdToken) => decodedToken.uid);
 }
 
 async function userFromRequest(request: Request): Promise<QueryDocumentSnapshot> {
-    return validateUid(request)
-        .then(uid => db.users.where("uid", "==", uid).limit(1))
-        .then(query => query.get())
-        .then(querySnapshot => querySnapshot.docs[0])
+  return validateUid(request)
+    .then((uid) => db.users.where("uid", "==", uid).limit(1))
+    .then((query) => query.get())
+    .then((querySnapshot) => querySnapshot.docs[0]);
 }
 
 // export const helloWorld = onRequest((request, response) => {
@@ -51,29 +51,42 @@ async function userFromRequest(request: Request): Promise<QueryDocumentSnapshot>
 //     response.send("Hello again !");
 // });
 
-export const addUser = onRequest(async (request, response) => {
-    const { uid, name, birth, expYears, email } = request.query as { uid: string, name: string, birth: string, expYears: string, email: string }
-    var user: User = { uid: uid, created: new Date(), name: name, birth: new Date(birth), expYears: parseInt(expYears), entries: [], tags: [] }
-    user = UserZ.parse({
-        ...user, ...(email && { email }),
-    })
-    db.users.add(user)
-        .then((res: DocumentReference) => res.get())
-        .then(user => response.send(user))
-        .catch((error: Error) => console.error("Caught error!", error))
+export const addUser = functions.auth.user().onCreate(async (user) => {
+  console.log("Add user triggered", user);
+  return await db.users.doc(user.uid).set({created: new Date()});
 });
 
-export const deleteUser = onRequest(async (request, response) => {
-    userFromRequest(request)
-        .then(user => user.ref.delete())
-        .then(_ => response.status(200).send("Deleted"))
-        .catch(error => response.status(500).send(`Error ${error}`))
+export const deleteUser = functions.auth.user().onDelete(async (user) => {
+  console.log("Delete user triggered", user);
+  return await db.users.doc(user.uid).delete();
 });
+
+export const editUserProfile = onRequest(async (request, response) => {
+  const {name, birth, expYears, email} = request.query as { name: string, birth: string, expYears: string, email: string };
+  validateUid(request)
+    .then((uid) => {
+      return {uid: uid, created: new Date(), name: name, birth: new Date(birth), expYears: parseInt(expYears), entries: [], tags: []};
+    })
+    .then((newUser) => UserZ.parse({...newUser, ...(email && {email})}))
+    .then((newUser) => db.users.add(newUser))
+    .then((res: DocumentReference) => res.get())
+    .then((user) => response.send(user))
+    .catch((error: Error) => console.error("Error adding user!", error));
+});
+
+// export const deleteUser = onRequest(async (request, response) => {
+//     userFromRequest(request)
+//         .then(user => user.ref.delete())
+//         .then(_ => response.status(200).send("Deleted"))
+//         .catch(error => response.status(500).send(`Error ${error}`))
+// });
 
 export const getUser = onRequest(async (request, response) => {
-    userFromRequest(request)
-        .then(user => UserZ.parse(user))
-        .then((user: User) => response.status(200).send(user))
-        .catch(error => response.status(500).send(`Error ${error}`))
+  userFromRequest(request)
+    .then((user) => {
+      console.log(user);
+      return UserZ.parse(user);
+    })
+    .then((user: User) => response.status(200).send(user))
+    .catch((error) => response.status(500).send(`Error ${error}`));
 });
-
